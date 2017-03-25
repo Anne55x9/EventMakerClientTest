147ec052afcfa6aea9a2485d003ee8d5a7031757
anne55x9 <anne55x9@edu.easj.dk>
2017-03-25 12:12:37 +0100

    Implemented DateTime datatype in Date property in Event class

--- EventMakerClientTest/Program.cs
+++ EventMakerClientTest/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EventMakerClientTest
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            const string serverUrl = "http://eventmakerws20170324112930.azurewebsites.net";
+
+            using (var client1 = new HttpClient())
+            {
+                client1.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                client1.BaseAddress = new Uri(serverUrl);
+                client1.DefaultRequestHeaders.Clear();
+
+                var myNewEvent = new Event()
+                {
+
+                    Name = "Abba",
+                    Description = "Svensk lækkerhed",
+                    Place = "EASJ",
+        
+        
+                    Date = "2017,05,10,12,00,00"
+                };
+
+                try
+                {
+                    var response = client1.PostAsJsonAsync<Event>("API/Hotels", myNewEvent).Result;
+                    Console.WriteLine("hotel : " + myNewEvent.Name + " hoteladresse : " + myNewEvent.Description);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Du har indsat et nyt hotel");
+                        Console.WriteLine("Post Content: " + response.Content.ReadAsStringAsync());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fejl, hotellet blev ikke indsat");
+                        Console.WriteLine("Statuskode : " + response.StatusCode);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Der er sket en fejl : " + e.Message);
+                }
+            }
+
         }
     }
 }
